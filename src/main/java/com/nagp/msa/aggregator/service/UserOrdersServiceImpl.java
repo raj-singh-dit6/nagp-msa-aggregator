a8cc9281ebe1ed49466f97fe201fd4b343cc6ddf
rajendrasingh <rajendrasingh@rajendrasingh.Nagarro.local>
2020-07-03 22:36:33 +0530

    urls moved to application.properties

--- src/main/java/com/nagp/msa/aggregator/service/UserOrdersServiceImpl.java
+++ src/main/java/com/nagp/msa/aggregator/service/UserOrdersServiceImpl.java
@@ -3,6 +3,7 @@ package com.nagp.msa.aggregator.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,20 +13,31 @@ import com.nagp.msa.aggregator.dto.UserOrdersDto;
 
 @Service
 public class UserOrdersServiceImpl implements UserOrdersService {
-	
+
 	private final static Logger LOG = LoggerFactory.getLogger(UserOrdersServiceImpl.class);
-	
+
+	@Value("${order.service.base.url}")
+	private String orderServiceBaseUrl;
+
+	@Value("${user.service.base.url}")
+	private String userServiceBaseUrl;
+
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Override
 	public UserOrdersDto getUserOrders(Integer userId) {
 		LOG.debug("inside UserOrdersServiceImpl.getUserOrders method");
-		UserDto user = restTemplate.getForObject("http://user-service:8050/user/"+userId,UserDto.class);
-		OrdersDto orders = restTemplate.getForObject("http://order-service:8060/order/"+userId,OrdersDto.class);
-		UserOrdersDto result=new UserOrdersDto();
-		result.setUserDetails(user);
-		result.setOrders(orders.getOrders());
+		UserOrdersDto result = null;
+		try {
+			UserDto user = restTemplate.getForObject(userServiceBaseUrl + "/user/" + userId, UserDto.class);
+			OrdersDto orders = restTemplate.getForObject(orderServiceBaseUrl + "/orders/" + userId, OrdersDto.class);
+			result = new UserOrdersDto();
+			result.setUserDetails(user);
+			result.setOrders(orders.getOrders());
+		} catch (Exception e) {
+			LOG.error("Could not find orders for provided user :"+userId +",  "+ e.getMessage());
+		}
 		return result;
 	}
 
